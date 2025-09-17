1c5054e9de9cce3e94ac2bd651abd4328dab72ec
bonellieric98-sys <bonellieric98@gmail.com>
2025-09-16 21:30:39 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD9SrWqQVL0_GxfBISBBezUOqQMbE_fiRU",
   authDomain: "invoicing-platform-f29ac.firebaseapp.com",
   projectId: "invoicing-platform-f29ac",
